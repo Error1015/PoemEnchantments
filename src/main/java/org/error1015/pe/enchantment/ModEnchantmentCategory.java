9f6f3cc23fcc36eea00467259df9f199d248d97c
Error1015 <156535541+Error1015@users.noreply.github.com>
2025-06-04 23:00:12 +0800

    添加工具函数

--- src/main/java/org/error1015/pe/enchantment/ModEnchantmentCategory.java
+++ src/main/java/org/error1015/pe/enchantment/ModEnchantmentCategory.java
@@ -1,10 +1,9 @@
 package org.error1015.pe.enchantment;
 
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import org.error1015.pe.util.CuriosUtil;
 
 public class ModEnchantmentCategory {
-    public static final EnchantmentCategory ANY = EnchantmentCategory.create("any", item -> item instanceof Item);
+    public static final EnchantmentCategory ANY = EnchantmentCategory.create("any", item -> true);
     public static final EnchantmentCategory CURIOS = EnchantmentCategory.create("curios", CuriosUtil::isCurioItem);
 }
--- src/main/java/org/error1015/pe/util/EnchantmentUtils.java
+++ src/main/java/org/error1015/pe/util/EnchantmentUtils.java
@@ -19,4 +19,35 @@ public class EnchantmentUtils {
         }
         return level;
     }
+
+    public static int getEnchantmentLevel(ItemStack stack, Enchantment enchantment) {
+        return stack.getAllEnchantments().getOrDefault(enchantment, 0);
+    }
+
+    /**
+     * 物品是否存在某一附魔
+     */
+    public static boolean isItemHasEnchantments(ItemStack item, Enchantment enchantment) {
+        return item.getAllEnchantments().containsKey(enchantment);
+    }
+
+    /**
+     * 任意装备含有某附魔
+     */
+    public static boolean isAnyEquipArmorHasEnchantment(Entity entity, Enchantment enchantment) {
+        for (ItemStack armorSlot : entity.getArmorSlots()) {
+            return isItemHasEnchantments(armorSlot, enchantment);
+        }
+        return false;
+    }
+
+
+    public static boolean isAllEquipArmorHasEnchantment(Entity entity, Enchantment enchantment) {
+        for (ItemStack armorSlot : entity.getArmorSlots()) {
+            if (!isItemHasEnchantments(armorSlot, enchantment)) {
+                return false;
+            }
+        }
+        return true;  // 所有盔甲都有该附魔
+    }
 }
