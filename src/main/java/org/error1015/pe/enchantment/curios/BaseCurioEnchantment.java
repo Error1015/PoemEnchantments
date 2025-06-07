475282c3826764f80917023eef066ba37ec5481a
Error1015 <156535541+Error1015@users.noreply.github.com>
2025-06-07 21:27:31 +0800

    e

--- src/main/java/org/error1015/pe/enchantment/curios/BaseCurioEnchantment.java
+++ src/main/java/org/error1015/pe/enchantment/curios/BaseCurioEnchantment.java
@@ -8,7 +8,6 @@ import org.error1015.pe.enchantment.PEEnchantmentHelper;
  */
 public class BaseCurioEnchantment extends Enchantment {
     public BaseCurioEnchantment(Rarity rarity) {
-        //                                                       或许这个东西在这里没有任何作用
         super(rarity, PEEnchantmentHelper.CURIOS, PEEnchantmentHelper.NOTHING);
     }
 }
