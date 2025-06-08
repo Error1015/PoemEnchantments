4b7c183a52137b618ce5897d6b5f4f6a1c258b16
Error1015 <156535541+Error1015@users.noreply.github.com>
2025-06-08 21:59:37 +0800

    l

--- src/main/java/org/error1015/pe/data/lang/ModZhCnLanguageProvider.java
+++ src/main/java/org/error1015/pe/data/lang/ModZhCnLanguageProvider.java
@@ -3,9 +3,7 @@ package org.error1015.pe.data.lang;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.LanguageProvider;
 import org.error1015.pe.enchantment.ModEnchantments;
-import org.error1015.pe.util.EnchantmentUtil;
 import org.error1015.pe.util.data.EnchantmentDescUtil;
-import org.error1015.pe.util.data.Lang;
 import org.jetbrains.annotations.NotNull;
 
 import static org.error1015.pe.PoemEnchantment.MODID;
